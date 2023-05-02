78a594ecf5e4bd70f27e69af1608fd7a1dc209bc
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-05-02 23:51:07 +0530

    Update

---
+++ index.js
@@ -0,0 +1,12 @@
+function hoverarrowl() {
+   document.getElementById("arrow_l").style.borderColor = "white"
+}
+function hoverarrowr() {
+   document.getElementById("arrow_r").style.borderColor = "white"
+}
+function nohoverarrowl() {
+   document.getElementById("arrow_l").style.borderColor = "black"
+}
+function nohoverarrowr() {
+   document.getElementById("arrow_r").style.borderColor = "black"
+}
