b181bda1b0a30320dcf9b57e6a7143d04f2f77cb
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-05-18 22:52:29 +0530

    Update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// let pstyle1 = backgroundColor = "rgb(63, 95, 109)"
+// let pstyle2 = borderTopColor = "#969696"
+let arrowl = document.getElementById('l_arrow');
+let arrowr = document.getElementById('r_arrow');
+
 function hoverarrowl() {
    document.getElementById("arrow_l").style.borderColor = "white"
 }
@@ -10,3 +15,69 @@ function nohoverarrowl() {
 function nohoverarrowr() {
    document.getElementById("arrow_r").style.borderColor = "black"
 }
+
+let poster = [
+   "Images/Demon S.jpg",
+   "Images/Dr Stone.webp",
+   "Images/JJK.jpg",
+   "Images/Demon S.jpg",
+   "Images/Ranking Kings.webp",
+   "Images/Cheat Skill in another world.webp"
+];
+
+let pinfo = [
+   document.getElementById('p0'),
+   document.getElementById('p1'),
+   document.getElementById('p2'),
+   document.getElementById('p3'),
+   document.getElementById('p4'),
+   document.getElementById('p5')
+];
+
+let getpost = document.getElementById('postimg');
+let i = 0;
+
+const changepost = (a) => {
+   if (a == 1) {
+      if (i < 5) {
+         ++i;
+         getpost.src = poster[i];
+      }
+   }
+   else {
+      if (i > 0) {
+         --i;
+         getpost.src = poster[i];
+      }
+   }
+   switch (i) {
+      case 0:
+         pinfo[0].style.backgroundColor = 'rgb(63, 95, 109)'
+         pinfo[0].style.borderTopColor = "#969696";
+         break;
+      case 1:
+         pinfo[1].style.backgroundColor = 'rgb(63, 95, 109)'
+         pinfo[1].style.borderTopColor = "#969696";
+         break;
+      case 2:
+         pinfo[2].style.backgroundColor = "rgb(63, 95, 109)"
+         pinfo[2].style.borderTopColor = "#969696";
+         break;
+      case 3:
+         pinfo[3].style.backgroundColor = "rgb(63, 95, 109)"
+         pinfo[3].style.borderTopColor = "#969696";
+         break;
+      case 4:
+         pinfo[4].style.backgroundColor = "rgb(63, 95, 109)"
+         pinfo[4].style.borderTopColor = "#969696";
+         break;
+      case 5:
+         pinfo[5].style.backgroundColor = "rgb(63, 95, 109)"
+         pinfo[5].style.borderTopColor = "#969696";
+         break;
+
+      default:
+         break;
+   }
+}
+console.log(i);
